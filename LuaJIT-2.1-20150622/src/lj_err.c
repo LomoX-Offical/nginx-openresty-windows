93f2d4be4330ada89f3295ac32374c1d55f4d74a
BloodyKnight <597905803@qq.com>
2015-08-11 15:12:10 +0800

    1、luajit 64位Lib 和 DLL 文件入库 2、为了使 windows x64 luajit 编译通过，修改lj_err.c源代码

--- LuaJIT-2.1-20150622/src/lj_err.c
+++ LuaJIT-2.1-20150622/src/lj_err.c
@@ -368,6 +368,36 @@ LJ_FUNCA int lj_err_unwind_arm(int state, void *ucb, _Unwind_Context *ctx)
 #include <windows.h>
 
 /* Taken from: http://www.nynaeve.net/?p=99 */
+typedef EXCEPTION_DISPOSITION (*PEXCEPTION_ROUTINE) (
+  struct _EXCEPTION_RECORD *ExceptionRecord,
+  PVOID EstablisherFrame,
+  struct _CONTEXT *ContextRecord,
+  PVOID DispatcherContext
+);
+
+#define UNWIND_HISTORY_TABLE_SIZE 12
+typedef struct _UNWIND_HISTORY_TABLE_ENTRY {
+  ULONG64 ImageBase;
+  PRUNTIME_FUNCTION FunctionEntry;
+} UNWIND_HISTORY_TABLE_ENTRY, *PUNWIND_HISTORY_TABLE_ENTRY;
+
+typedef struct _UNWIND_HISTORY_TABLE {
+  ULONG Count;
+  UCHAR Search;
+  ULONG64 LowAddress;
+  ULONG64 HighAddress;
+  UNWIND_HISTORY_TABLE_ENTRY Entry[UNWIND_HISTORY_TABLE_SIZE];
+} UNWIND_HISTORY_TABLE, *PUNWIND_HISTORY_TABLE;
+
+VOID NTAPI RtlUnwindEx(
+  PVOID TargetFrame,
+  PVOID TargetIp,
+  PEXCEPTION_RECORD ExceptionRecord,
+  PVOID ReturnValue,
+  PCONTEXT OriginalContext,
+  PUNWIND_HISTORY_TABLE HistoryTable
+);
+
 typedef struct UndocumentedDispatcherContext {
   ULONG64 ControlPc;
   ULONG64 ImageBase;
