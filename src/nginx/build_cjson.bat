1b41bdafd8b543eb97f7f0156c10ba9bb1ef5b80
zqzjz0911 <zqzjz1982@gmail.com>
2016-07-07 16:12:44 +0800

    add build cjson and redis parser dll.
    Signed-off-by: zqzjz0911 <zqzjz1982@gmail.com>

---
+++ src/nginx/build_cjson.bat
@@ -0,0 +1,5 @@
+SET ORG_DIR=%cd%
+cd /d %~dp0
+
+devenv ..\lua-cjson-2.1.0.4\cjson.vcproj /REBUILD "Release|Win32"
+copy /Y ..\lua-cjson-2.1.0.4\Release\cjson.dll objs\
---
+++ src/nginx/build_redis_parser_x64.bat
@@ -0,0 +1,5 @@
+SET ORG_DIR=%cd%
+cd /d %~dp0
+
+devenv ..\lua-redis-parser-0.12\redis-parser.vcproj /REBUILD "Release|x64"
+copy /Y ..\lua-redis-parser-0.12\x64\Release\redis_parser.dll objs\
