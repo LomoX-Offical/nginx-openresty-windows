2d145ee7afcce52cc74b778a7aa0ddf713ef44e5
zqzjz0911 <zqzjz1982@gmail.com>
2017-05-26 20:13:45 +0800

    make it build successfully
    Signed-off-by: zqzjz0911 <zqzjz1982@gmail.com>

--- src/nginx/build_cjson_x64.bat
+++ src/nginx/build_cjson_x64.bat
@@ -1,6 +1,6 @@
 SET ORG_DIR=%cd%
 cd /d %~dp0
 
-devenv ..\lua-cjson-2.1.0.4\cjson.vcproj /REBUILD "Release|x64"
-copy /Y ..\lua-cjson-2.1.0.4\x64\Release\cjson.dll objs\
-copy /Y ..\lua-cjson-2.1.0.4\x64\Release\cjson.pdb objs\
+devenv ..\lua-cjson-2.1.0.5\cjson.vcproj /REBUILD "Release|x64"
+copy /Y ..\lua-cjson-2.1.0.5\x64\Release\cjson.dll objs\
+copy /Y ..\lua-cjson-2.1.0.5\x64\Release\cjson.pdb objs\
