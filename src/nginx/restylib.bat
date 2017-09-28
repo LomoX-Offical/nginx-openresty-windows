40d6ff39624d500af3e9ce9cd53236987a3167e3
zqzjz0911 <zqzjz1982@gmail.com>
2017-09-28 16:34:12 +0800

    commit the makefile
    Signed-off-by: zqzjz0911 <zqzjz1982@gmail.com>

--- src/nginx/restylib.bat
+++ src/nginx/restylib.bat
@@ -1,16 +1,16 @@
 SET ORG_DIR=%cd%
 set LUA_LIB_DIR=objs\lua
-set CORE_LIB=..\lua-resty-core-0.1.11\
-set DNS_LIB=..\lua-resty-dns-0.18\
-set LIMIT_TRAFFIC_LIB=..\lua-resty-limit-traffic-0.03\
-set LOCK_LIB=..\lua-resty-lock-0.06\
+set CORE_LIB=..\lua-resty-core-0.1.12\
+set DNS_LIB=..\lua-resty-dns-0.19\
+set LIMIT_TRAFFIC_LIB=..\lua-resty-limit-traffic-0.04\
+set LOCK_LIB=..\lua-resty-lock-0.07\
 set LRUCACHE_LIB=..\lua-resty-lrucache-0.06\
 set MEMCACHED_LIB=..\lua-resty-memcached-0.14\
-set MYSQL_LIB=..\lua-resty-mysql-0.19\
+set MYSQL_LIB=..\lua-resty-mysql-0.20\
 set REDIS_LIB=..\lua-resty-redis-0.26\
-set STRING_LIB=..\lua-resty-string-0.09\
+set STRING_LIB=..\lua-resty-string-0.10\
 set UPLOAD_LIB=..\lua-resty-upload-0.10\
-set UPSTREAM_HEALTHCHECK_LIB=..\lua-resty-upstream-healthcheck-0.04\
+set UPSTREAM_HEALTHCHECK_LIB=..\lua-resty-upstream-healthcheck-0.05\
 set WEBSOCKET_LIB=..\lua-resty-websocket-0.06\
 
 cd /d %~dp0
